e6c6ad060fd8c240c9a716dc92cda48438bef84b
Anton Petrov <nesamurai@yandex.ru>
2022-05-26 22:42:42 +0300

    trying to make templates for lesson 3

--- ex.py
+++ ex.py
@@ -1,5 +1,15 @@
 from framework.wsgi import Framework
+from jinja2 import Environment, PackageLoader, select_autoescape
+
 from urls import my_urls
 
 
 app = Framework(my_urls)
+
+env = Environment(
+    loader=PackageLoader("app"),
+    autoescape=select_autoescape()
+)
+
+template = env.get_template("base.html")
+print(template.render())
